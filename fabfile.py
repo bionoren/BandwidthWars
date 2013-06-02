f904fe0f28be6c51f36137705fc5d33ab28fb023
Drew Crawford <drew@sealedabstract.com>
2013-06-02 17:12:21 -0500

    working on deploys

---
+++ fabfile.py
@@ -0,0 +1,26 @@
+from fabric.api import run, sudo, env, settings, local
+from fabric.context_managers import cd
+import fabric
+import scissors
+env.use_ssh_config = True
+DEBIAN = "debian"
+
+WORKING_TREE="bandwidthwars"
+
+def server_install():
+    if scissors.what_system() == DEBIAN:
+        run('apt-get update')
+        run('apt-get -y install sudo')
+        scissors.debian_upgrade()
+    else:
+        pass
+    scissors.install_python()
+    scissors.install_git()
+    scissors.install_python_worker_tools()
+    print "server is now setup; you need code_drop and a git push to begin"
+
+def code_drop():
+	scissors.code_drop(WORKING_TREE)
+
+def emergency_start():
+	scissors.emergency_start(WORKING_TREE)
